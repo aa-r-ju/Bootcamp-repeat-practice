abccaac35121b65615b4962bb36c47e103ff5687
aa-r-ju <aarju3447@gmail.com>
2024-02-15 23:52:21 +0545

    fix:fix the error

--- pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
+++ pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
@@ -1,15 +1,16 @@
-let boolean1 = true, boolean2 = true;
+// let boolean1 = true, boolean2 = true;
 // let boolean1 = true, boolean2 = false;
 // let boolean1 = false, boolean2 = true;
-// let boolean1 = false, boolean2 = false;
+let boolean1 = false, boolean2 = false;
 
 let result;
 
 // YOUR CODE BELOW
 if(boolean1 === true && boolean2 === true){
     result = "both"
-} else if (boolean1 === true  && boolean2 === false || boolean1 === false && boolean2 === true ) {
+} else if ((boolean1 === true  && boolean2 === false) ||( boolean1 === false && boolean2 === true) ) {
     result = "one"
-} else if (boolean1 === false || boolean2 === false) {
+} else if (boolean1 === false && boolean2 === false) {
     result = "none"
 }
+console.log(result)
