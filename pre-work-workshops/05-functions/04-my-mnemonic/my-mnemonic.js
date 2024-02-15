bf43ef02f6ec46fd049457afd66257b32e8028d9
aa-r-ju <aarju3447@gmail.com>
2024-02-16 00:49:48 +0545

    feat:completed my-mnemonic

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,10 @@
-// YOUR CODE BELOW
+function myMnemonic(...words) {
+    const initials = words.map(word => word.charAt(0));
+
+    const mnemonic = initials.join('');
+
+    return mnemonic;
+}
+
+console.log(myMnemonic('Must', 'Accept', 'Sugar', 'Happiness')); 
+console.log(myMnemonic('We', 'Eat', 'Eggs')); 
