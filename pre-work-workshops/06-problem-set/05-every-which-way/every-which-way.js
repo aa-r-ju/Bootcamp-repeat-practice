cc2f336cb7f811cf45d50b2a1a60e15ee0618810
aa-r-ju <aarju3447@gmail.com>
2024-02-16 11:45:35 +0545

    feat:completed every-which-way

--- pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
+++ pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+function everyWhichWay(num1,num2,num3) {
+    if((num1+num2) === num3) {
+        return "sum"
+    } else if ((num1-num2) === num3) {
+        return "difference"
+    } else if ((num1*num2) === num3) {
+        return "product"
+    } else if ((num1/num2) === num3) {
+        return "fraction"
+    } else {
+        return null
+    }
+
+}
+everyWhichWay(10, 20, 30)
+everyWhichWay(50, 20, 30)
+everyWhichWay(4, 4, 16)
+everyWhichWay(100, 10, 10)
+everyWhichWay(1, 1000, 50)
