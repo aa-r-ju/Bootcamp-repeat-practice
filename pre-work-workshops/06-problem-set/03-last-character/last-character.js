6fc1c3b6c544b8ea13b99ef4a1b86b7dcf6fbf02
aa-r-ju <aarju3447@gmail.com>
2024-02-16 11:33:41 +0545

    feat:completed max-of-three

--- pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function lastCharacter(val1,val2) {
+    if(val1.charAt(val1.length-1)=== (val2.charAt(val2.length-1))) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+lastCharacter('apples', 'pumpkins')
+lastCharacter('marker', 'pen')
--- pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,10 @@
-// YOUR CODE BELOW
+function maxOfThree(num1, num2, num3) {
+    let maxNum = (num1 > num2) ? num1 : num2;
+
+    maxNum = (maxNum > num3) ? maxNum : num3;
+
+    return maxNum;
+}
+
+maxOfThree(5, 4, 10); 
+maxOfThree(7, 7, 4);  
